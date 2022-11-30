119f1de51bb5bd86999bbeccc9a0d1e95006e435
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-11-30 03:51:27 +0300

    feat: add encodeToRot13

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -229,8 +229,8 @@ function getRectangleString(width, height) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  return str.replace(/[A-Za-z]/g, (c) => String.fromCharCode(c.charCodeAt(0) + (c.toUpperCase() <= 'M' ? 13 : -13)));
 }
 
 /**
@@ -246,8 +246,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 
